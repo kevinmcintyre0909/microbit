9e74e02eedd620cb7cc639b500b2d579dd5fdb51
kevinmcintyre0909 <121754256+kevinmcintyre0909@users.noreply.github.com>
2023-01-01 12:37:11 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,79 @@
+basic.showString("Hi Amelia!")
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . . #
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . #
+    . . . # #
+    . . . # #
+    . . . . #
+    . . . . #
+    `)
+basic.showLeds(`
+    . . . # .
+    . . # # .
+    . . # # #
+    . . . # #
+    . . . # .
+    `)
+basic.showLeds(`
+    . . # . .
+    . # # . .
+    . # # # #
+    . . # # #
+    . . # . #
+    `)
+basic.showLeds(`
+    . # . . #
+    # # . . #
+    # # # # #
+    . # # # .
+    . # . # .
+    `)
+basic.showLeds(`
+    . # . . #
+    # # . . #
+    # # # # #
+    . # # # .
+    . # . # .
+    `)
+basic.showLeds(`
+    # . . # .
+    # . . # .
+    # # # # .
+    # # # . .
+    # . # . .
+    `)
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    # # # . .
+    # # . . .
+    . # . . .
+    `)
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    # # . . .
+    # . . . .
+    # . . . .
+    `)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+music.playMelody("C5 B A G G A B D ", 120)
